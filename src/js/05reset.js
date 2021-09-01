80593b233ee25aec64fd083d87fb7019d775859e
MariviEscudero <mariviesmo@gmail.com>
2021-09-01 17:54:36 +0200

    add server js folder

--- src/js/05reset.js
+++ src/js/05reset.js
@@ -1,6 +1,7 @@
 const resetFormButton = document.querySelector('.js_resetbutton');
-const inputReset = document.querySelector('.js_formtoreset'); 
+const inputReset = document.querySelector('.js_form'); 
 function handleResetFormButton(){
     inputReset.reset();
 }
 resetFormButton.addEventListener('click', handleResetFormButton);
+
