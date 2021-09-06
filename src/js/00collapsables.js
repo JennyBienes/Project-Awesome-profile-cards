0167585034373045a5e4089747aeb296f6e16853
IreneBioque <irenebioque@gmail.com>
2021-09-06 13:38:40 +0200

    add arrows collapsables

--- src/js/00collapsables.js
+++ src/js/00collapsables.js
@@ -5,26 +5,25 @@ const headerCollapsables = document.querySelectorAll('.js_headerCollapsable');
 
 for (const headerCollapsable of headerCollapsables) {
     headerCollapsable.addEventListener('click', handlerCollapsable);
-}
+};
 
 function handlerCollapsable(ev) {
     const clickedHeader = ev.currentTarget;
-    const clickedContent = clickedHeader.nextElementSibling;
-    // clickedContent.classList.toggle('hidden');
-    console.log(clickedHeader);
-    console.log(clickedContent);
+  const clickedContent = clickedHeader.nextElementSibling;
+  const arrow = clickedHeader.querySelector('.js_arrow');
 
     for (const collapsable of contentCollapsable) {
-        // debugger;
           if (clickedContent === collapsable) {
               collapsable.classList.toggle('hidden');
-               collapsable.classList.toggle('div_content');
+              collapsable.classList.toggle('div_content');
+            arrow.classList.toggle('fa-chevron-down');
+              arrow.classList.toggle('fa-chevron-up');
           } else {
               collapsable.classList.add('hidden');
-              collapsable.classList.remove('div_content');
+            collapsable.classList.remove('div_content');
+         
+            
       }
   }
+  
 }
-
-
-// headerCollapsable.addEventListener('click', handlerCollapsable);
