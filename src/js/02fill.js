18642f7960202ead3b5a4f86d66c1a88a3e31c38
MariviEscudero <mariviesmo@gmail.com>
2021-09-06 19:08:52 +0200

    fix all fill functions

--- src/js/02fill.js
+++ src/js/02fill.js
@@ -1,38 +1,30 @@
-//Acción para el nombre///
-const nameElement = document.querySelector('.js_name');
-const namePreviewElement = document.querySelector('.js_name_cards');
-function handleName() {
-  if (nameElement.value !== '') {
-    console.log(nameElement.value);
-    namePreviewElement.innerHTML = nameElement.value;
-  }
-}
-nameElement.addEventListener('change', handleName);
+//Acción para el nombre y el trabajo
+//variables
+const inputTextElements = document.querySelectorAll('.js_inputtext');
+const textPreviewElements = document.querySelectorAll('.js_previewtext');
+function handlePreviewText() {
 
-//Acción para trabajo//
-const jobElement = document.querySelector('.js_input_job');
-const jobPreviewElement = document.querySelector('.js_job');
-function handleJob() {
-  if (jobElement.value !== '') {
-    jobPreviewElement.innerHTML = jobElement.value;
-  }
-}
+  textPreviewElements[0].innerHTML = inputTextElements[0].value;
+  textPreviewElements[1].innerHTML = inputTextElements[1].value;
 
-jobElement.addEventListener('change', handleJob);
+}
+for (const inputTextElement of inputTextElements) {
+  inputTextElement.addEventListener('change', handlePreviewText);
+}
 
 //ACCIONES PARA LOS LINKS
 
 //variables
+
 const inputCardsLinks = document.querySelectorAll('.js_input_link');
 console.log(inputCardsLinks);
 const cardPreviewIcons = document.querySelectorAll('.js-icon-circle');
 
-
-//preventDefault de los enlaces de la tarjeta, hacer solo una funcion y un listener!!!!!
+//preventDefault de los enlaces de la tarjeta
 
 function handleClickCardLinks(event) {
   let iconListened = event.currentTarget;
-  if (iconListened.href === "http://localhost:3000/cards.html"||iconListened.href === "tel:"||iconListened.href === "mailto:") {
+  if (iconListened.href === 'http://localhost:3000/cards.html') {
     iconListened = event.preventDefault();
   }
 }
@@ -42,49 +34,19 @@ for (const cardPreviewIcon of cardPreviewIcons) {
 
 //funcion unica para los links
 
-function handleCardLinks(event) {
- 
-  for (let index = 0; index < inputCardsLinks.length; index++) {
-    let indexValue = event.currentTarget.value;
-    console.log(indexValue);
-    if (indexValue !== '') {
-       for (const cardPreviewIcon of cardPreviewIcons){
-        const cardPreview = cardPreviewIcon;
-        cardPreview.setAttribute('href', indexValue);
-        }
-    }
-  }
+function handleCardLinks() {
+  cardPreviewIcons[1].setAttribute('href','mailto:' + inputCardsLinks[0].value);
+
+  cardPreviewIcons[0].setAttribute('href', 'tel:+34' + inputCardsLinks[1].value);
+
+  cardPreviewIcons[2].setAttribute('href', inputCardsLinks[2].value);
+
+  cardPreviewIcons[3].setAttribute('href','https://github.com/' + inputCardsLinks[3].value);
 }
 for (const inputCardsLink of inputCardsLinks) {
   inputCardsLink.addEventListener('change', handleCardLinks);
 }
 
-//fin de funcion  unica para los links
 
-//Acción para email
 
-/*function handleMail() {
-  if (emailElement.value !== '') {
-    emailPreviewElement.setAttribute('href', 'mailto:' + emailElement.value);
-  }
-}
-emailElement.addEventListener('change', handleMail);
 
-//Acción para linkedin
-
-function handleLinkedin() {
-  linkedinPreviewElement.event.preventDefault();
-  if (linkedinElement.value === '') {
-    linkedinPreviewElement.setAttribute('href', linkedinElement.value);
-  }
-}
-linkedinElement.addEventListener('change', handleLinkedin);
-
-//Acción para github
-
-function handleGithub() {
-  if (githubElement.value !== '') {
-    githubPreviewElement.setAttribute('href', githubElement.value);
-  }
-}
-githubElement.addEventListener('change', handleGithub);*/
