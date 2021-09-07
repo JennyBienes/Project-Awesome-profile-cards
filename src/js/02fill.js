be6d5a447977b6d9bbcfb1e7d64eac50d2234808
IreneBioque <irenebioque@gmail.com>
2021-09-07 14:14:03 +0200

    add share js

--- src/js/02fill.js
+++ src/js/02fill.js
@@ -9,7 +9,9 @@ const profilePreview = document.querySelector('.js__profile-preview');
 function getImage(e){
   const myFile = e.currentTarget.files[0];
   fr.addEventListener('load', writeImage);
-  fr.readAsDataURL(myFile);
+  const imageForFetch = fr.readAsDataURL(myFile);
+  return imageForFetch;
+
 }
 
 function writeImage() {
