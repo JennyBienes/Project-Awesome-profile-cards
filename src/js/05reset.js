d09743193ae04fc8b7061535579a012d4e71f52e
MariviEscudero <mariviesmo@gmail.com>
2021-09-14 18:23:26 +0200

    Add form styles

--- src/js/05reset.js
+++ src/js/05reset.js
@@ -1,7 +1,22 @@
 const resetFormButton = document.querySelector('.js_resetbutton');
-const inputReset = document.querySelector('.js_form'); 
+/*const inputReset = document.querySelector('.js_form'); 
 function handleResetFormButton(){
     inputReset.reset();
 }
-resetFormButton.addEventListener('click', handleResetFormButton);
+resetFormButton.addEventListener('click', handleResetFormButton);*/
 
+function handleResetLocalStorage(){
+    localStorage.removeItem('dataUser');
+    location.reload();
+}
+resetFormButton.addEventListener('click', handleResetLocalStorage)
+
+/*function handleFavoritesResetBtn(){
+    const arrayLength = favoriteShows.length;
+    favoriteShows.splice(0,arrayLength);
+    localStorage.removeItem('favoriteShows');
+    localStorage.removeItem('favoriteSelectedShows');
+    removeClassSelected();
+    paintFavorites();
+  }
+  favoritesResetBtn.addEventListener('click', handleFavoritesResetBtn);*/
