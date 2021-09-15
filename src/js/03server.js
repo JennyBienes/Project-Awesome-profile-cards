487adb7a224e400d0469d7f62b9570bfe007dbaf
IreneBioque <irenebioque@gmail.com>
2021-09-15 18:30:32 +0200

    add buttons share linkedin and facebook

--- src/js/03server.js
+++ src/js/03server.js
@@ -3,6 +3,8 @@ const parrafoShare = document.querySelector(".js_parrafo_share");
 const sectionShare = document.querySelector(".js_share_twitter");
 const shareTitle = document.querySelector(".js_sharetitle");
 const linkTwitter = document.querySelector(".js_linktwitter");
+const linkFacebook = document.querySelector(".js_linkFacebook");
+const linkLinkedin = document.querySelector(".js_linkLinkedin");
 let dataUser = {
   palette: "",
   name: "",
@@ -40,6 +42,8 @@ function handleCreateBtn(ev) {
         parrafoShare.innerHTML = data.cardURL;
         parrafoShare.setAttribute("href", data.cardURL);
         linkTwitter.href = `https://twitter.com/intent/tweet?url=${data.cardURL};`
+        linkFacebook.href = `https://www.facebook.com/sharer/sharer.php?u=${data.cardURL}`
+        linkLinkedin.href = `https://www.linkedin.com/shareArticle?url=${data.cardURL}`
 
       } else {
         shareTitle.innerHTML = "Error al crear tarjeta:";
